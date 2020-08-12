841769f39b7f9bc45d5778ea22c1cae22f206bc2
Luis Mazariego <mazariego2011@gmail.com>
2020-08-11 23:12:37 -0600

    Added basic client side validations

--- JobBoard.WebApp/Models/JobViewModel.cs
+++ JobBoard.WebApp/Models/JobViewModel.cs
@@ -1,17 +1,26 @@
 ﻿namespace JobBoard.WebApp.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class JobViewModel
     {
         public int Job { get; set; }
 
+        [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
         public string JobTitle { get; set; }
 
+        [Required]
+        [MinLength(5)]
+        [MaxLength(1000)]
         public string Description { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime ExpiresAt { get; set; }
     }
 }
